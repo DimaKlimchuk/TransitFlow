80f9f62ff267adddd650ed1d1dd63e47c94f341f
Dmytro Klymchuk <dima.klimchuk.2016@gmail.com>
2024-05-14 00:34:48 +0300

    Created server

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+
+// Вказуємо Express використовувати папку 'public' для статичних файлів
+app.use(express.static("public"));
+
+// Стартовий маршрут ('/') буде автоматично відображати index.html
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/Home.html");
+});
+
+// Маршрут для сторінки "Про нас"
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/public/About.html");
+});
+
+// Маршрут для сторінки "Послуги"
+app.get("/services", (req, res) => {
+  res.sendFile(__dirname + "/public/Services.html");
+});
+
+// Маршрут для сторінки "Команда"
+app.get("/team", (req, res) => {
+  res.sendFile(__dirname + "/public/Team.html");
+});
+
+// Маршрут для сторінки "Контакти"
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/public/Contact.html");
+});
+
+// Обробник маршруту 404
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + "/public/404.html");
+});
+
+// Стартуємо сервер
+app.listen(3000, () => {
+  console.log("Сервер стартував на порті 3000!");
+});
